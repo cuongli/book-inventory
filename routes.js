00e0ced6c8d9c7bfe4f1e727eeda92505cd8ec5f
ficutruo <cuong.van.truong@finn.no>
2016-03-09 10:53:00 +0100

    minor changes

--- routes.js
+++ routes.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var router = express.Router();
 
 
 module.exports = function (bookRepository) {
@@ -7,7 +6,26 @@ module.exports = function (bookRepository) {
         getCount: function (req, res) {
             bookRepository.getCount(req.params.isbn).then(function (result) {
                 if (result !== null) {
-                    res.status(200).json({count: result});
+                    //res.status(200).json({count: result});
+                    res.format({
+                        //If the header is not specified, invoke this
+                        'text/plain': function(){
+                            res.send('Plain text');
+                        },
+
+                        'text/html': function(){
+                            res.send('<p>available</p>');
+                        },
+
+                        'application/json': function(){
+                            res.send({ title: 'Yolo!', subtitle: 'More then you needed', thumbnail: 'thumbnail', type: 'application/json' });
+                        },
+                        //no match is found
+                        'default': function() {
+                            // log the request and respond with 406
+                            res.status(406).send('Not Acceptable');
+                        }
+                    });
                 } else {
                     res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
                 }
