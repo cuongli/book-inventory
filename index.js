925f471bc8fd7452d3891a029dd6e5ddd85461bb
ficutruo <cuong.van.truong@finn.no>
2016-03-10 16:07:04 +0100

    fix failed build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var bookRepository = require('./bookRepository')();
 var auth = require('./authentication');
-var app = require('./app.js')(bookRepository, auth('admin', 'admin'));
+//var app = require('./app.js')(bookRepository, auth('admin', 'admin'));
+var app = require('./app.js')(bookRepository);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
